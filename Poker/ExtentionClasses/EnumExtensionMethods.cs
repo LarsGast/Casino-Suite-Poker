cb93c8f5e7c04648fbb5b89cf21c0a07172a51ea
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2023-07-26 15:51:42 +0200

    Rename ExtentionMethods.cs to EnumExtensionMethods.cs and put the file in a seperate folder. This paves the way for different types of extention classes.

--- Poker/ExtentionClasses/EnumExtensionMethods.cs
+++ Poker/ExtentionClasses/EnumExtensionMethods.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Poker {
-	public static class ExtensionMethods {
+	public static class EnumExtensionMethods {
 
 		/// <summary>
 		/// Extension method.
