b5417dfdccfbbd44834c27e99cf3800b7a448087
Lars Hoekstra <73757191+LarsGast@users.noreply.github.com>
2023-07-26 15:45:24 +0200

    Adding a test project to the repo. It already contains a test for asserting the shuffle of the deck is random (uniform).

---
+++ Unit-Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Unit-Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Unit-Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6688d787-22e0-4df6-9dd5-8313e1e90b61")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Unit-Tests/UnitTestsDeck.cs
@@ -0,0 +1,76 @@
+﻿using Casino_Suite_Poker;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using MathNet.Numerics.Distributions;
+using Accord.Statistics.Distributions.Univariate;
+using System.Linq;
+
+namespace Unit_Tests {
+	[TestClass]
+	public class UnitTestsDeck {
+
+		private const int NumberOfShuffles = 1000; // Number of times to shuffle the deck
+
+		/// <summary>
+		/// Tests whether the deck shuffle is actually random.
+		/// So: the distribution of cards is uniform.
+		/// </summary>
+		[TestMethod]
+		public void shuffleIsRandom() {
+
+			// Initiate some variables.
+			var random = new Random();
+			var deck = Deck.get();
+
+			// Dictionary to store the frequency of each card after shuffling.
+			var cardFrequency = new Dictionary<Card, int>();
+			foreach (var card in deck.cards) {
+				cardFrequency[card] = 0;
+			}
+
+			// Perform shuffling multiple times and count the frequency of each card.
+			for (int i = 0; i < NumberOfShuffles; i++) {
+
+				// Include the random variable so that the default doesn't take the same seed.
+				deck.shuffle(random);
+
+				// Store which index the each card is shuffled.
+				int cardIndex = 0;
+				foreach (var card in deck.cards) {
+					cardFrequency[card] = cardFrequency[card] + cardIndex;
+					cardIndex++;
+				}
+			}
+
+			// Calculate the expected average card index after shuffling (assuming a uniform distribution).
+			double averageIndex = (deck.cards.Count - 1) / 2.0;
+
+			// Calculate the chi-square statistic
+			double chiSquare = 0;
+			foreach (var card in deck.cards) {
+
+				// The observer average index is the observed indexes summed up divided by the number of shuffles.
+				double observedAverageIndex = (double)cardFrequency[card] / NumberOfShuffles;
+
+				// Calculate the chi-square contribution for this card.
+				double chiSquareContribution = Math.Pow(observedAverageIndex - averageIndex, 2) / averageIndex;
+				chiSquare += chiSquareContribution;
+			}
+
+			// Degrees of freedom for a deck of cards is (number of cards - 1).
+			int degreesOfFreedom = deck.cards.Count - 1;
+
+			// Set the significance level (alpha) for the chi-square test.
+			// 0.05 here means we want to be 95% certain the distibution is uniform.
+			double alpha = 0.05;
+
+			// Use chi-square distribution table to get the critical value for the given significance level and degrees of freedom.
+			ChiSquared chiSquaredDist = new ChiSquared(degreesOfFreedom);
+			double criticalValue = chiSquaredDist.InverseCumulativeDistribution(1 - alpha);
+
+			// Perform the chi-square test.
+			Assert.IsFalse(chiSquare >= criticalValue, "The deck shuffle is not random.");
+		}
+	}
+}
