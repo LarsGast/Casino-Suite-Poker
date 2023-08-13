7e7094697cf20ae3a709f141c82e4a4f97ca9cad
Lars Hoekstra <larshoekstra14@gmail.com>
2023-08-13 20:57:33 +0200

    Changing Program.cs to have a new, better example.

--- Poker-Console/Program.cs
+++ Poker-Console/Program.cs
@@ -5,135 +5,142 @@ using Poker.WinningHands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Poker_Console {
 	internal class Program {
 		static void Main(string[] args) {
-			
-			// Get a deck and shuffle it.
+
+			// Define the possible players.
+			var playerNames = new List<string>() {
+				"Luz",
+				"Eda",
+				"King",
+				"Amity",
+				"Willow",
+				"Gus",
+				"Hunter",
+				"Vee"
+			};
+
+			// Define how many players will participate in the game.
+			var numberOfPlayers = 3;
+
+			// No more players than the number of names.
+			if (numberOfPlayers > playerNames.Count) {
+				throw new Exception("Not enough player names");
+			}
+
+			if (numberOfPlayers < 2) {
+				throw new Exception("At least 2 players should be participating.");
+			}
+
+			// Make a player for each player name.
+			var players = new List<Player>();
+			for (int i = 0; i < numberOfPlayers; i++) {
+				players.Add(new Player(playerNames[i]));
+			}
+
+			// Get the deck and shuffle it.
 			var deck = Deck.get();
 			deck.shuffle();
 
-			// Initiate some variables.
-			var pocketPlayerOne = new List<Card>();
-			var pocketPlayerTwo = new List<Card>();
-			var pocketPlayerThree = new List<Card>();
-			var table = new List<Card>();
-
-			// Deal cards to the three players.
+			// Draw 2 cards for each player.
 			for (int i = 0; i < 2; i++) {
-				pocketPlayerOne.Add(deck.draw());
-				pocketPlayerTwo.Add(deck.draw());
-				pocketPlayerThree.Add(deck.draw());
-			}
-
-			// Burn a card.
-			deck.draw();
-
-			// Deal the flop.
-			table.Add(deck.draw());
-			table.Add(deck.draw());
-			table.Add(deck.draw());
-
-			// Burn a card.
-			deck.draw();
-
-			// Deal the turn.
-			table.Add(deck.draw());
-
-			// Burn a card.
-			deck.draw();
-
-			// Deal the river.
-			table.Add(deck.draw());
-
-			// Display what each player has and what is on the table.
-			Console.WriteLine($"Player 1: {pocketPlayerOne.getDisplayString()}");
-			Console.WriteLine($"Player 2: {pocketPlayerTwo.getDisplayString()}");
-			Console.WriteLine($"Player 3: {pocketPlayerThree.getDisplayString()}");
-			Console.WriteLine($"Table: {table.getDisplayString()}");
-
-			// Determine the best hand each player can make.
-			var handPlayerOne = PokerHand.getBestHand(table.Union(pocketPlayerOne));
-			var handPlayerTwo = PokerHand.getBestHand(table.Union(pocketPlayerTwo));
-			var handPlayerThree = PokerHand.getBestHand(table.Union(pocketPlayerThree));
-
-			Console.WriteLine("");
-
-			// Display the best hand each player can make.
-            Console.WriteLine($"Hand player 1:\n" +
-				$"\tHand Type: {handPlayerOne.handType}\n" +
-				$"\tHighest value for this hand type: {(handPlayerOne.firstCardValue != null ? handPlayerOne.firstCardValue.Value.getDisplayString() : null)}\n" +
-				$"\tSecond highest value for this hand type: {(handPlayerOne.secondCardValue != null ? handPlayerOne.secondCardValue.Value.getDisplayString(): null)}\n" +
-				$"\tKickers: {handPlayerOne.kickers.getDisplayString()}"
-			);
-
-			Console.WriteLine("");
-
-			Console.WriteLine($"Hand player 2:\n" +
-				$"\tHand Type: {handPlayerTwo.handType}\n" +
-				$"\tHighest value for this hand type: {(handPlayerTwo.firstCardValue != null ? handPlayerTwo.firstCardValue.Value.getDisplayString() : null)}\n" +
-				$"\tSecond highest value for this hand type: {(handPlayerTwo.secondCardValue != null ? handPlayerTwo.secondCardValue.Value.getDisplayString() : null)}\n" +
-				$"\tKickers: {handPlayerTwo.kickers.getDisplayString()}"
-			);
-
-			Console.WriteLine("");
-
-			Console.WriteLine($"Hand player 3:\n" +
-				$"\tHand Type: {handPlayerThree.handType}\n" +
-				$"\tHighest value for this hand type: {(handPlayerThree.firstCardValue != null ? handPlayerThree.firstCardValue.Value.getDisplayString() : null)}\n" +
-				$"\tSecond highest value for this hand type: {(handPlayerThree.secondCardValue != null ? handPlayerThree.secondCardValue.Value.getDisplayString() : null)}\n" +
-				$"\tKickers: {handPlayerThree.kickers.getDisplayString()}"
-			);
-
-			Console.WriteLine("");
-
-			// Determine who wins.
-			// ToDo: Refactor this in a static method in PokerHand.cs
-			var playerOneWinsAgainstPlayerTwo = handPlayerOne.winsAgainst(handPlayerTwo);
-			if (playerOneWinsAgainstPlayerTwo == true) {
-				var playerOneWinsAgainstPlayerThree = handPlayerOne.winsAgainst(handPlayerThree);
-
-				if (playerOneWinsAgainstPlayerThree == true) {
-					Console.WriteLine("Player 1 wins!");
-				}
-				else if (playerOneWinsAgainstPlayerThree == false) {
-					Console.WriteLine("Player 3 wins!");
-				}
-				else if (playerOneWinsAgainstPlayerThree == null) {
-					Console.WriteLine("It's a draw between players 1 en 3!");
-				}
-			}
-			else if (playerOneWinsAgainstPlayerTwo == false) {
-				var playerTwoWinsAgainstPlayerThree = handPlayerTwo.winsAgainst(handPlayerThree);
-
-				if (playerTwoWinsAgainstPlayerThree == true) {
-					Console.WriteLine("Player 2 wins!");
-				}
-				else if (playerTwoWinsAgainstPlayerThree == false) {
-					Console.WriteLine("Player 3 wins!");
-				}
-				else if (playerTwoWinsAgainstPlayerThree == null) {
-					Console.WriteLine("It's a draw between players 2 en 3!");
-				}
-			}
-			else if (playerOneWinsAgainstPlayerTwo == null) {
-				var playerThreeWinsAgainstPlayerOne = handPlayerThree.winsAgainst(handPlayerOne);
-
-				if (playerThreeWinsAgainstPlayerOne == true) {
-					Console.WriteLine("Player 3 wins!");
-				}
-				else if (playerThreeWinsAgainstPlayerOne == false) {
-					Console.WriteLine("It's a draw between players 1 en 2!");
-				}
-				else if (playerThreeWinsAgainstPlayerOne == null) {
-					Console.WriteLine("It's a draw between players 1, 2, en 3!");
+				foreach (var player in players) {
+					player.addCard(deck.draw());
 				}
 			}
 
+			// Draw 5 cards on the table.
+			var tableCards = new List<Card>();
+			for (int i = 0; i < 5; i++) {
+				tableCards.Add(deck.draw());
+			}
+
+			// Determine the best hand for each player.
+			foreach (var player in players) {
+				player.setPokerHand(tableCards);
+			}
+
+			// Get the best hand amongst all the players.
+			var bestHand = PokerHand.getWinningHand(players.Select(player => player.hand));
+
+			// Get all the players that draw with the best hand.
+			// More than one player can have the best hand.
+			var winningPlayers = players.Where(player => player.hand.winsAgainst(bestHand) == null).ToList();
+
+			// Display everything.
+			Console.WriteLine("Table:");
+			Console.WriteLine(tableCards.getDisplayString());
+			Console.WriteLine("");
+
+			foreach (var player in players) {
+				Console.WriteLine($"Name: {player.name}");
+				Console.WriteLine($"Cards: {player.cards.getDisplayString()}");
+				Console.WriteLine($"Hand Type: {player.hand.handType}");
+				Console.WriteLine($"Highest value for this hand type: {(player.hand.firstCardValue != null ? player.hand.firstCardValue.Value.getDisplayString() : null)}");
+				Console.WriteLine($"Second highest value for this hand type: {(player.hand.secondCardValue != null ? player.hand.secondCardValue.Value.getDisplayString() : null)}\n" +
+					$"Kickers: {player.hand.kickers.getDisplayString()}");
+				Console.WriteLine("");
+			}
+
+			Console.WriteLine("Winning player(s):");
+			foreach (var player in winningPlayers) {
+				Console.WriteLine(player.name);
+			}
+
+			// ReadKey so the app doesn't close.
 			Console.ReadKey();
+		}
+	}
+
+	/// <summary>
+	/// Possible Player class.
+	/// </summary>
+	internal class Player {
+		
+		/// <summary>
+		/// Name of the player.
+		/// </summary>
+		public string name { get; set; }
+
+		/// <summary>
+		/// The cards that the player gets dealt.
+		/// </summary>
+		public List<Card> cards { get; set; }
+
+		/// <summary>
+		/// The best possible hand considering the cards the player received and the cards on the table.
+		/// </summary>
+		public PokerHand hand { get; set; }
+
+		/// <summary>
+		/// Constructor.
+		/// </summary>
+		/// <param name="name"></param>
+		public Player(string name) {
+			this.name = name;
+			this.cards = new List<Card>();
+		}
+
+		/// <summary>
+		/// Add a card to the players hand.
+		/// </summary>
+		/// <param name="card"></param>
+		public void addCard(Card card) {
+			cards.Add(card);
+		}
+
+		/// <summary>
+		/// Set the pokerHand property to the best possible hand the player can make.
+		/// </summary>
+		/// <param name="tableCards"></param>
+		public void setPokerHand(List<Card> tableCards) {
+			this.hand = PokerHand.getBestHand(tableCards.Union(this.cards));
 		}
 	}
 }
